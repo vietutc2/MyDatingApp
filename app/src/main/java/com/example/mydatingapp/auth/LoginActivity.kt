ae3f3e84d350cef3839b34a6a1bf08c400e4a841
vietutc <viethm22@gmail.com>
2022-12-04 12:40:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mydatingapp/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mydatingapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mydatingapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
---
+++ app/src/main/java/com/example/mydatingapp/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mydatingapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mydatingapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
